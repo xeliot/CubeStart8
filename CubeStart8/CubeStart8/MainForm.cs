1430a5466264e61814f640e09219b4fd1f35b362
daveho8888 <daveho8888@gmail.com>
2016-04-23 14:28:42 -0700

    icon changing has been implemented

--- CubeStart8/CubeStart8/MainForm.cs
+++ CubeStart8/CubeStart8/MainForm.cs
@@ -40,6 +40,8 @@ namespace CubeStart8
                 if (DialogResult.OK == openFileDialog1.ShowDialog())
                 {
                     paths[pictureBox1.Name] = openFileDialog1.FileName;
+                    Icon ico = Icon.ExtractAssociatedIcon(@openFileDialog1.FileName);
+                    pictureBox1.Image = ico.ToBitmap();
                 }
             }
             else {
