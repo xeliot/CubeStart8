c4244c6f0430849a2771c8db1e30828bca77e4a7
daveho8888 <daveho8888@gmail.com>
2016-04-23 15:01:43 -0700

    Icons update

--- CubeStart8/CubeStart8/MainForm.cs
+++ CubeStart8/CubeStart8/MainForm.cs
@@ -260,6 +260,8 @@ namespace CubeStart8
                 if (DialogResult.OK == openFileDialog1.ShowDialog())
                 {
                     paths[pictureBox13.Name] = openFileDialog1.FileName;
+                    Icon ico = Icon.ExtractAssociatedIcon(@openFileDialog1.FileName);
+                    pictureBox13.Image = ico.ToBitmap();
                 }
             }
             else
